5b10ae8ead1cd5d23d896a92f534d508823d4e15
nobody <no80dy@gmail.com>
2023-04-16 19:48:47 +0300

    feature: add initial functionality.

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi_users import FastAPIUsers
+
+from auth.auth import auth_backend
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+from auth.database import User
+
+
+app = FastAPI()
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
